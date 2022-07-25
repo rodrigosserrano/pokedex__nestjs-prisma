68ee6824523e840cc29fd85e1e179557cbbebe84
rodrigo.serrano <rodrigo.serrano@bomvalor.com.br>
2022-07-25 18:46:25 -0300

    feat: prisma initial config

--- src/messages/MessageDTO.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsNotEmpty } from 'class-validator';
-//Validação se o campo está vindo da forma esperada
-export class MessageDTO {
-  @IsString()
-  @IsNotEmpty()
-  text: string;
-}
